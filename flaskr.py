8e9fd4971e8bf2c961b4e8f6dc57c319267d944e
Alvin Qin <qinwen923@gmail.com>
2015-05-13 15:49:33 +0800

    setup

--- flaskr.py
+++ flaskr.py
@@ -9,19 +9,24 @@ def check_signature(token):
     signature = request.args.get("signature")
     timestamp = request.args.get("timestamp")
     nonce = request.args.get("nonce")
-    tmp_list = [token, timestamp, nonce]
-    tmp_list.sort()
-    tmp_str = "%s%s%s" % tuple(tmp_list)
-    hash_str = hashlib.sha1(tmp_str.encode('utf-8')).hexdigest()
-    if hash_str == signature:
-        return True
-    else:
+    try:
+        tmp_list = [token, timestamp, nonce]
+        tmp_list.sort()
+        tmp_str = "%s%s%s" % tuple(tmp_list)
+        hash_str = hashlib.sha1(tmp_str.encode('utf-8')).hexdigest()
+        if hash_str == signature:
+            return True
+        else:
+            return False
+    except TypeError as e:
+        print(str(e))
+    finally:
         return False
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    echo_str = request.args.get("echoStr")
+    echo_str = request.args.get("echostr")
     if echo_str is not None:
         if check_signature(TOKEN):
             return echo_str
