eeb46ce05a39cccbf641d86b7dac50aeca61689d
Alvin Qin <qinwen923@gmail.com>
2015-05-14 17:22:16 +0800

    setup

--- flaskr.py
+++ flaskr.py
@@ -1,5 +1,8 @@
 import hashlib
-from flask import Flask, request
+from lxml import etree
+from flask import Flask, request, render_template
+import time
+from lib.WXBizMsgCrypt import WXBizMsgCrypt
 
 TOKEN = 'weixin'
 app = Flask(__name__)
@@ -23,16 +26,61 @@ def check_signature(token):
         return False
 
 
+def msg_handler():
+    encodingAESKey = "mLGUf7FULWK5e0kECMlf8hw4XiE3s3S2Wc0cLUyiSyW"
+    to_xml = """
+    <xml>
+        <ToUserName><![CDATA[oia2TjjewbmiOUlr6X-1crbLOvLw]]></ToUserName>
+        <FromUserName><![CDATA[gh_7f083739789a]]></FromUserName>
+        <CreateTime>1407743423</CreateTime>
+        <MsgType><![CDATA[video]]></MsgType>
+        <Video>
+            <MediaId><![CDATA[eYJ1MbwPRJtOvIEabaxHs7TX2D-HV71s79GUxqdUkjm6Gs2Ed1KF3ulAOA9H1xG0]]></MediaId>
+            <Title><![CDATA[testCallBackReplyVideo]]></Title>
+            <Description><![CDATA[testCallBackReplyVideo]]></Description>
+        </Video>
+    </xml>
+    """
+    token = "weixin"
+    signature = request.args.get("signature")
+    timestamp = request.args.get("timestamp")
+    nonce = request.args.get("nonce")
+    appid = "wx1c16712b3ad9ffa8"
+
+    # 测试加密接口
+    encryp_test = WXBizMsgCrypt(token, encodingAESKey, appid)
+    ret, encrypt_xml = encryp_test.EncryptMsg(to_xml, nonce)
+    return ret,
+
+
+def post():
+    str_xml = request.data
+    xml = etree.fromstring(str_xml)
+    content = xml.find("Content").text + " handled by python."
+    msg_type = xml.find("MsgType").text
+    to_user = xml.find("FromUserName").text
+    from_user = xml.find("ToUserName").text
+    view_obj = {'content': content,
+                'msg_type': msg_type,
+                'from_user': from_user,
+                'to_user': to_user,
+                'create_time': int(time.time())
+                }
+    return render_template("reply_text.xml", view_obj=view_obj)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     echo_str = request.args.get("echostr")
+    # echostr校验
     if echo_str is not None:
         if check_signature(TOKEN):
             return echo_str
         else:
             return 'ok'
+    # 消息处理
     else:
-        return ''
+        return post()
 
 
 if __name__ == '__main__':
